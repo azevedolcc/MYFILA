cd3cbb31b5c017cf032c496c9c6d6792bcc4c0fc
Sergio Murillo <sergiomcavalcante@gmail.com>
2018-08-04 17:58:36 -0300

    Commit 04/08/2018

--- src/pages/acessopage/acessopage.ts
+++ src/pages/acessopage/acessopage.ts
@@ -14,6 +14,7 @@ import { CadUsuPage } from '../../pages/cadusupage/cadusupage';
 export class AcessoPage {
 
   usuario: any;
+  //data: any;
 
   public usuemail: string;  
   public usupassword: string;  
@@ -33,20 +34,33 @@ export class AcessoPage {
     console.log('ionViewDidLoad AcessoPage');
   }
 
-  acessar(usuemail, usupassword ){
-    console.log( "Logar ===> " + this.usuemail + "->" + this.usupassword);
+  acessar(usuemail, usupassword) {
+
+    let toast = this.toastCtrl.create({duration: 3000, position: 'middle'});
+
+    console.log( "Logar ===> " + this.usuemail + " -> " + this.usupassword);
     if (this.usuemail !== null && this.usupassword !== null) {
       console.log( "Vou chamar o metodo para Logar ===> " )
-      this.usuProvider.loginUsuario(usuemail, usupassword); 
+      this.usuProvider.getEmail(this.usuemail).then((data) => {
+              this.usuario = data
+              console.log("Voltei do getEmail: " + data +  this.usuemail)
+              console.log(this.usuario[0].email + ' ' + this.usuario[0].password)
+              if (this.usupassword !== this.usuario[0].password) {
+                    toast.setMessage("Encontramos problemas no seu login. Verifique a senha e tente novamente.");
+                    toast.present(); 
+              } else {
+                    toast.setMessage("Usuário logado com sucesso.");
+                    toast.present();
+                    this.navCtrl.setRoot(HomePage);
+              }       
+          })
+          .catch((error: any) => {
+                   toast.setMessage("Não foi encontrado usuário com login " + this.usuemail);
+                   toast.present();          
+          })
     }
-    
-    //this.navCtrl.setRoot(HomePage);
-  } 
-
-  //cadusu(){
-  //  this.navCtrl.push(CadUsuPage);
-  //}
-
+  }
+  
   cadUsu(){
      this.navCtrl.push(CadUsuPage);
   }
